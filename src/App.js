a42f58dc8aa4d2493960860e5c317ff41da7ddb6
Madhu Rebbana <madhu.rebbana@gmail.com>
2019-06-13 02:12:24 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react"
 import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css"
-import Leaderboard from './containers/Leaderboard.js'
 import LandingPage from './containers/LandingPage.js'
 import HackShackCarousel from './containers/HackShackCarousel.js'
 
