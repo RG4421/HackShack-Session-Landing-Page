09d7018075f4ff8ba026712baa36599279f9d3cf
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2020-05-27 21:31:57 -0600

    add cardimage component

---
+++ src/components/Cards/CardImage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Box } from 'grommet';
+
+export const CardImage = ({ background, children, ...rest }) => {
+  return (
+    <Box
+      background={background}
+      height="small"
+      round="xsmall"
+      overflow="hidden"
+      basis="1/2"
+      style={{ position: 'relative' }}
+      {...rest}
+    >
+      {children}
+    </Box>
+  );
+};
+
+CardImage.defaultProps = {
+  background: 'background-back',
+};
+
+CardImage.propTypes = {
+  children: PropTypes.node,
+  background: PropTypes.string,
+};
