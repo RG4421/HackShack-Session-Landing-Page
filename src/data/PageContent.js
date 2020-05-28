013db441da1ff92513ed076c91adce310b30fc48
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2020-05-27 21:33:09 -0600

    add content data file

---
+++ src/data/PageContent.js
@@ -0,0 +1,56 @@
+export const communityContent = [
+  {
+    image: '/img/SlackLogo.svg',
+    title: 'Sign up for the HPE Developer Newsletter',
+    desc:
+      'Got questions or just want to chat with other folks that work at or with HPE?',
+    link: 'https://slack.hpedev.io/',
+    label: 'Join News Letter',
+  },
+  {
+    image: '/img/SlackLogo.svg',
+    title: 'Join the HPE Dev Slack Community',
+    desc: 'consectetur adipiscing elit, sed do eiusmod tempor incididunt ut',
+    link: 'https://slack.hpedev.io/',
+    label: 'Join Slack Channel',
+  },
+  {
+    image: '/img/SlackLogo.svg',
+    title: 'Sign up for the HPE Developer Newsletter',
+    desc: 'consectetur adipiscing elit, sed do eiusmod tempor incididunt ut',
+    link: 'https://developer.hpe.com/newsletter-signup',
+    label: 'Join Newsletter',
+  },
+  {
+    image: '/img/SlackLogo.svg',
+    title: 'Sign up for the HPE Developer Newsletter',
+    desc: 'consectetur adipiscing elit, sed do eiusmod tempor incididunt ut',
+    link: 'https://developer.hpe.com/newsletter-signup',
+    label: 'Join Newsletter',
+  },
+];
+
+export const arcadeContent = [
+  {
+    image: '/img/SlackLogo.svg',
+    title: 'Sign up for the HPE Developer Newsletter',
+    desc:
+      'Got questions or just want to chat with other folks that work at or with HPE?',
+    link: 'https://slack.hpedev.io/',
+    label: 'Join News Letter',
+  },
+  {
+    image: '/img/SlackLogo.svg',
+    title: 'Join the HPE Developer Slack Community',
+    desc: 'consectetur adipiscing elit, sed do eiusmod tempor incididunt ut',
+    link: 'https://slack.hpedev.io/',
+    label: 'Join Slack Channel',
+  },
+  {
+    image: '/img/SlackLogo.svg',
+    title: 'Sign up for the HPE Developer Newsletter',
+    desc: 'consectetur adipiscing elit, sed do eiusmod tempor incididunt ut',
+    link: 'https://developer.hpe.com/newsletter-signup',
+    label: 'Join Newsletter',
+  },
+];
