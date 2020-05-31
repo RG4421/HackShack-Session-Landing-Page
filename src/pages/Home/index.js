2e1c2c4ef08e9063176197d2e245d843ef015d12
pramod sareddy <reddypramod85>
2020-05-31 18:42:40 -0500

    rotate text and button by 10 deg

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,19 +5,29 @@ import { Layout, ButtonSplit } from '../../components/index';
 const Home = () => {
   return (
     <Layout background="/img/hack-shack-home-background.png">
-      <Box direction="column" align="center">
+      <Box direction="column" align="end">
         <Box>
-          <Image src="/img/hack-shack-dve-logo.png" alt="Hack Shack" />
+          <Image
+            fit="contain"
+            src="/img/hack-shack-dve-logo.png"
+            alt="Hack Shack"
+          />
         </Box>
-        <Box>
-          <Text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dapibus
-            enim ante, a efficitur lacus lacinia facilisis. Pellentesque mattis
-            tellus aliquam, faucibus diam vitae, faucibus nibh. Proin semper
-            interdum leo, ut tempus tortor consectetur nec.
+
+        <Box
+          align="center"
+          gap="medium"
+          style={{
+            transform: 'rotate(-10deg)',
+            width: '720px',
+            height: '168px',
+          }}
+        >
+          <Text alignSelf="end" textAlign="start" size="xlarge">
+            Welcome to the underbelly of HPE Discover, welcome to the Dev
+            HackShack. Events, talks, and games for the folks that make the
+            awesome possible. Come on in and see who’s home!
           </Text>
-        </Box>
-        <Box>
           <ButtonSplit to="https://developer.hpe.com">
             Visit HPE Developer
           </ButtonSplit>
