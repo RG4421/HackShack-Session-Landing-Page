3f84cef43cfa991e24fc5d309ea72d557a27cb1d
pramod sareddy <reddypramod85>
2020-06-01 16:46:51 -0500

    remove header nav links

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,25 +1,8 @@
 /* (C) Copyright 2019 Hewlett Packard Enterprise Development LP. */
-import React, { useContext } from 'react';
-import {
-  Box,
-  Text,
-  Header as HeaderGrommet,
-  Image,
-  Button,
-  ResponsiveContext,
-} from 'grommet';
+import React from 'react';
+import { Box, Text, Header as HeaderGrommet, Image } from 'grommet';
 
 export const Header = () => {
-  const headerLinks = [
-    { label: 'HOME', href: '#' },
-    { label: 'AGENDA', href: '#' },
-    { label: 'TOPICS', href: '#' },
-    { label: 'CONNECT', href: '#' },
-    { label: 'SHARE', href: '#' },
-    { label: 'PLAY', href: '#' },
-    { label: 'HELP', href: '#' },
-  ];
-  const size = useContext(ResponsiveContext);
   return (
     <HeaderGrommet
       align="center"
@@ -49,17 +32,6 @@ export const Header = () => {
           <Text weight="bold">HPE</Text>
           <Text>Developer</Text>
         </Box>
-      </Box>
-      <Box
-        direction="row"
-        alignSelf="start"
-        align={size !== 'small' ? 'center' : undefined}
-        gap="xsmall"
-        wrap
-      >
-        {headerLinks.map(link => (
-          <Button key={link.label} label={link.label} />
-        ))}
       </Box>
     </HeaderGrommet>
   );
