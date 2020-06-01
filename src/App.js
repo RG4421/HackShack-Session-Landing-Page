3b6926bd5666587087e32821f48eb037d65d112c
pramod sareddy <reddypramod85>
2020-06-01 10:50:15 -0500

    add google analytics

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
 import { Grommet } from 'grommet';
 import { hpe } from 'grommet-theme-hpe';
 import { Home, Community, Arcade } from './pages/index';
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
+import { throwError } from 'rxjs';
 
 const App = () => {
+  let gtagId;
+  let gaDebug;
+  if (process.env.NODE_ENV === 'production') {
+    gtagId = 'UA-108944070-5';
+    gaDebug = false;
+  } else if (process.env.NODE_ENV === 'development') {
+    console.log('dev mode ga', process.env.NODE_ENV);
+    gtagId = 'UA-NNNNNN-N';
+    gaDebug = false;
+  } else {
+    throwError(
+      "NODE_ENV not set to 'production' nor 'development'." +
+        'Google Analytics tracking will not be initialized.',
+    );
+  }
+  ReactGA.initialize(gtagId, {
+    debug: gaDebug,
+  });
+  const history = createBrowserHistory();
+  history.listen(location => {
+    ReactGA.set({ page: location.pathname });
+    ReactGA.pageview(location.pathname);
+  });
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const { location } = window;
+      ReactGA.set({ page: location.pathname });
+      ReactGA.pageview(location.pathname);
+    }
+  }, []);
   return (
     <Grommet theme={hpe} themeMode="dark" full>
-      <Router>
+      <Router history={history}>
         <Switch>
           <Route exact path="/">
             <Home />
