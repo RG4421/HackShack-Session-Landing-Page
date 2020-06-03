2a01c815cdf3878e9977314860a4f7b5bfe6d9fd
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2020-06-03 11:44:39 -0600

    add schedule page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Home from './Home';
 import HackShackAttack from './HackShackAttack';
 import Community from './Community';
 import Arcade from './Arcade';
+import Schedule from './Schedule';
 import StickerWall from './StickerWall';
 
-export { Home, HackShackAttack, Community, Arcade, StickerWall };
+export { Home, HackShackAttack, Community, Arcade, StickerWall, Schedule };
